fe87078bd2ce12f3687d13418cbb0653b5c1edae
sngillca <sngill.ca@gmail.com>
2020-11-06 19:38:27 -0800

    Natalia Gill first commit

--- app/src/main/java/com/example/budgetbuddy115v1/MainActivity.java
+++ app/src/main/java/com/example/budgetbuddy115v1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TAG:", "Hello world");
+        Log.d("TAG:", "Hello world this is Natalia Gill");
     }
 }
