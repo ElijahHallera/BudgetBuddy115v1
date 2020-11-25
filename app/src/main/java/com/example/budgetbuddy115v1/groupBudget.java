e064b514bb31b278582957171d11188e2eb80a25
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2020-11-24 23:48:45 -0800

    changes to my branch

--- app/src/main/java/com/example/budgetbuddy115v1/groupBudget.java
+++ app/src/main/java/com/example/budgetbuddy115v1/groupBudget.java
@@ -1,6 +1,5 @@
 package com.example.budgetbuddy115v1;
 
-<<<<<<< HEAD
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,17 +13,4 @@ public class groupBudget extends AppCompatActivity {
     }
 
 }
-=======
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class groupBudget extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_budget);
-    }
-}
->>>>>>> 27e6a97edfcee627da409416acd83f6fecd9fe57
