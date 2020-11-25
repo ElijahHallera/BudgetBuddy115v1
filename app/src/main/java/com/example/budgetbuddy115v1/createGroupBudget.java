6024b9801e93c0cf242d86b6bd54accfda06e94f
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2020-11-24 18:46:10 -0800

    adding stuff for for group budgetting

--- app/src/main/java/com/example/budgetbuddy115v1/createGroupBudget.java
+++ app/src/main/java/com/example/budgetbuddy115v1/createGroupBudget.java
@@ -1,24 +1,36 @@
 package com.example.budgetbuddy115v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class budgetSetUp extends AppCompatActivity {
+public class createGroupBudget extends AppCompatActivity {
     EditText budgetName, tAmount, sAmount;
     Button creating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.groupbugetsetup);
+        setContentView(R.layout.activity_create_group_budget);
 
         budgetName = findViewById(R.id.budgName);
         tAmount = findViewById(R.id.totalAmount);
         sAmount = findViewById(R.id.startingAmount);
         creating = findViewById(R.id.create);
+
+        creating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), groupBudget.class));
+
+            }
+        });
+
     }
 
 }
