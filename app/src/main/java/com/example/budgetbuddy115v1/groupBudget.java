a81339a18a0b02a24152b5cd0841d1fff1316275
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2020-12-11 08:18:58 -0800

    savestate

--- app/src/main/java/com/example/budgetbuddy115v1/groupBudget.java
+++ app/src/main/java/com/example/budgetbuddy115v1/groupBudget.java
@@ -15,14 +15,21 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class groupBudget extends AppCompatActivity {
 
     private TextView text;
     private Button goHome;
+    private Button update;
+    private TextView newExpenses;
+    private TextView newDeposits;
     //in order to add a way to go to groubbudget page
     //private Button goToBudgets;
 
@@ -37,6 +44,7 @@ public class groupBudget extends AppCompatActivity {
     String mainName;
     int totalAmount;
     int totalSaved;
+    FirebaseFirestore fstore;
 
 
     @Override
@@ -50,10 +58,13 @@ public class groupBudget extends AppCompatActivity {
         }
 
 
+        fstore = FirebaseFirestore.getInstance();
 
+        update = findViewById(R.id.update);
         goHome = findViewById(R.id.Home);
         text = findViewById(R.id.budgetName);
         text.setText(mainName);
+
 
         setupPieChart();
 
@@ -68,6 +79,36 @@ public class groupBudget extends AppCompatActivity {
             }
         });
 
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DocumentReference documentReference = fstore.collection("groupBudget").document(mainName);
+                String dummy = documentReference.getPath();
+                Toast.makeText(groupBudget.this, "" + dummy, Toast.LENGTH_SHORT).show();
+                Bundle extra = getIntent().getExtras();
+                if(extra != null){
+                    budget = extra.getFloat("total");
+                    savings = extra.getFloat("savings");
+                    expenses = extra.getFloat("expenses");
+
+                }
+                newExpenses = findViewById(R.id.spentMoney);
+                newDeposits = findViewById(R.id.deposit);
+                float trigger = expenses;
+                float trigger2 = Float.valueOf(newExpenses.getText().toString());
+                float adding = trigger + trigger2;
+                String sub = "currentExpenses";
+                Map<String, Object>updates = new HashMap<>();
+                updates.put("currentExpenses", adding);
+
+                //DocumentReference documentReference = fstore.collection("groupBudget").document(mainName);
+                documentReference.update(updates);
+
+                reSetUpPieChart(adding);
+
+
+            }
+        });
 
     }
 
@@ -105,6 +146,40 @@ public class groupBudget extends AppCompatActivity {
         chart.setEntryLabelTextSize(14);
         chart.getDescription().setEnabled(false);
 
+    }
+
+    private void reSetUpPieChart(float expence){
+        Bundle extra = getIntent().getExtras();
+        if(extra != null){
+            budget = extra.getFloat("total");
+            savings = extra.getFloat("savings");
+            //expenses = extra.getFloat("expenses");
+
+        }
+        leftOver = budget - (expence + savings);
+        salaryText = String.format("%.2f", budget);
+        float budgetSalary[] = {expence, leftOver, savings};
+        String budgetNames[] = {"expenses", "leftOver", "savings"};
+        //budgetSalary[] = {expenses, leftOver, savings};
+
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for(int i = 0; i < budgetSalary.length; i++){
+            pieEntries.add(new PieEntry(budgetSalary[i], budgetNames[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries, "");
+        dataSet.setValueTextSize(20f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        PieChart chart = findViewById(R.id.piechart);
+        chart.setData(data);
+        chart.setCenterText("$" + salaryText);
+        chart.setCenterTextSize(25);
+        chart.animateY(1000);
+        chart.invalidate();
+        chart.setEntryLabelTextSize(14);
+        chart.getDescription().setEnabled(false);
     }
 
 }
